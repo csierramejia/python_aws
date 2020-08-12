dddee9c76dfc5c426dfc08025b6b0a1c66d49f2e
Luis Fernando Hernandez <hluisfernando1@gmail.com>
2020-08-12 09:58:22 -0500

    testing with cors

--- api/app/app.py
+++ api/app/app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_cors import CORS
 from blueprints import (
     rekognition,
     sns
@@ -8,10 +7,8 @@ from blueprints import (
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    # app.config['CORS_HEADERS'] = 'Content-Type'
     app.register_blueprint(rekognition.bp)
     app.register_blueprint(sns.bp)
-    CORS(app)
 
 
     return app
--- api/app/blueprints/sns.py
+++ api/app/blueprints/sns.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, request
 from api.sns_api import SnsApi
-from flask_cors import CORS, cross_origin
 
 bp = Blueprint(
     "sns", __name__, url_prefix="/registro/sns"
@@ -8,22 +7,19 @@ bp = Blueprint(
 
 
 @bp.route("/send_code_register", methods=["POST"])
-@cross_origin()
 def send_code_register():
     return SnsApi(request).send_code_register()
 
 
 @bp.route("/valid_code/<code>", methods=["GET"])
-@cross_origin()
 def valid_code(code):
     return SnsApi(request).valid_code(code)
 
 
 
 @bp.route("/", methods=["GET"])
-@cross_origin()
 def helloWorld():
-  return "Hello, cross-origin-world!"
+  return "Hello, cross-origin-world cors enabled python!"
 
 
 
